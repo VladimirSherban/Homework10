f74232983bc57b69692760dd3fa307009bc2e254
VladimirSherban <Vovan_Sh_94@mail.ru>
2022-12-16 18:03:55 +0300

    Homework done!

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        task2();
+    }
+
+    public static void task1() {
+        System.out.println("Задача 1-2");
+        String firstName = " Ivan";
+        String middleName = " Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + firstName + middleName;
+        System.out.println(fullName);
+        System.out.println(fullName.toUpperCase());
+    }
+
+    public static void task2() {
+        System.out.println("\nЗадача 3");
+        String fullName = "Иванов Семён Семёнович";
+        fullName = fullName.replace('ё', 'e');
+        System.out.println(fullName);
     }
 }
